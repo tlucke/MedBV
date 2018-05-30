3ebae50c7b7c052c24ca548fa674e4f5a4a011c6
tlucke <thorsten.lucke@st.ovgu.de>
2018-05-30 09:24:50 +0200

    Add files via upload

---
+++ next_seeds_02.py
@@ -0,0 +1,59 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon May 28 17:24:46 2018
+
+@author: Thorsten
+"""
+
+import pydicom
+import cv2 as cv
+import numpy as np
+
+def next_seeds(previmg, prevseg, nextimg):
+	'''
+	sets new seeds for a new layer of DICOM data by comparing a given previous layer, using 
+	its segmentation, with its successor.
+	to be used for region growing.
+	INPUT: 	-previous layer + previous layer segmentation
+				-next layer
+	OUTPUT:  -array with seed coordinates
+	'''
+	
+	seed_arr = list()
+	
+	''' 
+	convert images previmg, prevseg and nextimg 
+	to arrays 320x320
+	'''
+	previmgarr = np.array(previmg.pixel_array)
+	prevsegarr = np.array(prevseg.pixel_array)
+	nextimgarr = np.array(nextimg.pixel_array)
+	
+	'''
+	for: iterate through the segmentation
+		if seg_color_Value = 3: (3 equals the color of the liver in the given DICOM dataset)
+			if prev_coord ><= next_coord (given threshold)
+				put the coordinate into the seed_arr
+			elif
+				continue
+	'''
+	
+	threshold = 1 #TODO: Play around with threshold for possible better results
+	for i in range(320):
+		for j in range(320):
+			if prevsegarr[i,j] == 3:
+				
+				if abs(previmgarr[i,j]-nextimgarr[i,j]) < threshold:
+					#print([i,j])
+					seed_arr.append([i,j])
+	
+	return seed_arr
+
+'''TEST RANGE'''
+prev = pydicom.dcmread("medbv_data/P01/img0020.dcm")
+prevseg = pydicom.dcmread("medbv_seg/P01/img0020.dcm")
+nxt = pydicom.dcmread("medbv_data/P01/img0021.dcm")
+
+newseeds = next_seeds(prev, prevseg, nxt)
+print(newseeds)
+
