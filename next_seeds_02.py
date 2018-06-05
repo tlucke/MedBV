a30e682e02a5fed461c71058b1d8cddc5771d82d
tlucke <thorsten.lucke@st.ovgu.de>
2018-06-05 13:22:22 +0200

    Add files via upload

--- next_seeds_02.py
+++ next_seeds_02.py
@@ -25,15 +25,16 @@ def next_seeds(previmg, prevseg, nextimg):
 	convert images previmg, prevseg and nextimg 
 	to arrays 320x320
 	'''
-	"""
+	'''
 	previmgarr = np.array(previmg.pixel_array)
 	prevsegarr = np.array(prevseg.pixel_array)
 	nextimgarr = np.array(nextimg.pixel_array)
-	"""
+	'''
+	
 	previmgarr = np.array(previmg)
 	prevsegarr = np.array(prevseg)
 	nextimgarr = np.array(nextimg)
-
+	
 	'''
 	for: iterate through the segmentation
 		if seg_color_Value = 3: (3 equals the color of the liver in the given DICOM dataset)
@@ -43,24 +44,28 @@ def next_seeds(previmg, prevseg, nextimg):
 				continue
 	'''
 	
-	threshold = 3 #TODO: Play around with threshold for possible better results
-	for j in range(320):       #change to image shape
-		for i in range(320):
-			#if prevsegarr[i,j] == 1:
-			if prevsegarr[i,j] >= 1:
-				
-				if abs(previmgarr[i,j]-nextimgarr[i,j]) < threshold:
-					#print([i,j])
-					seed_arr.append([i,j, previmgarr[i,j]])
+	#threshold = 3 #TODO: Play around with threshold for possible better results
 	
+	n = 5	#block size
+	m = int(n/2)
+	for j in range(m, 320-m):       #change to image shape
+		for i in range(m, 320-m):
+			tmp = [[prevsegarr[k][h] for k in range(i-m,i+m+1)] for h in range(j-m,j+m+1)]
+			
+			#tmp = prevsegarr[(i-m) : (i+m+1), (j-m) : (j+m+1)]
+			#print(tmp[i,j])
+			if( np.prod(tmp) != 0 ):
+				seed_arr.append([i, j, previmgarr[i,j]])
+				prevsegarr[i,j] = 0
+			
 	return seed_arr
 
 '''TEST RANGE'''
-"""
+'''
 prev = pydicom.dcmread("medbv_data/P01/img0020.dcm")
 prevseg = pydicom.dcmread("medbv_seg/P01/img0020.dcm")
 nxt = pydicom.dcmread("medbv_data/P01/img0021.dcm")
 
 newseeds = next_seeds(prev, prevseg, nxt)
 print(newseeds)
-"""
+'''
